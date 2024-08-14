22fe1e25a93186817414fa9b0eb392b7c39ca867
Abdulaziz <wwxeww1wwxeww@gmail.com>
2024-08-14 18:43:40 +0500

    created

---
+++ src/main/java/up/pdp/apprecipes/AppRecipesApplication.java
@@ -0,0 +1,13 @@
+package up.pdp.apprecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRecipesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/up/pdp/apprecipes/AppRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package up.pdp.apprecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
