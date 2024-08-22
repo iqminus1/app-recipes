0bc4ff3fd36a2082b9a440cd4116aa7b9fcfa39c
ツ <javaguru1716@gmail.com>
2024-08-23 00:13:56 +0500

    Category is done and added all controllers

--- src/main/java/up/pdp/apprecipes/repository/AttachmentRepository.java
+++ src/main/java/up/pdp/apprecipes/repository/AttachmentRepository.java
@@ -1,5 +1,6 @@
 package up.pdp.apprecipes.repository;
 
+import io.micrometer.common.lang.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import up.pdp.apprecipes.exceptions.NotFoundException;
@@ -9,7 +10,8 @@ import java.util.UUID;
 
 @Repository
 public interface AttachmentRepository extends JpaRepository<Attachment, UUID> {
-    default Attachment getById(UUID id) {
+    @NonNull
+    default Attachment getById(@NonNull UUID id) {
         return findById(id).orElseThrow(() -> NotFoundException.errorById("Attachment", id));
     }
 
