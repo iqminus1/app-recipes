3a4e356ac7dd2816d37634a679d19e10a8d2b970
ツ <javaguru1716@gmail.com>
2024-08-24 16:29:58 +0500

    Optimized path in AppConst

--- src/main/java/up/pdp/apprecipes/exceptions/NotFoundException.java
+++ src/main/java/up/pdp/apprecipes/exceptions/NotFoundException.java
@@ -14,4 +14,8 @@ public class NotFoundException extends RuntimeException {
     public static NotFoundException errorById(String className, Object id) {
         return new NotFoundException(className + " not found by id -> " + id);
     }
+
+    public static NotFoundException error(String m) {
+        return new NotFoundException(m + " not found");
+    }
 }
--- src/main/java/up/pdp/apprecipes/utils/AppConst.java
+++ src/main/java/up/pdp/apprecipes/utils/AppConst.java
@@ -5,5 +5,5 @@ import java.nio.file.Path;
 
 public interface AppConst {
     String API_V1 = "/api/v1";
-    Path BASE_PATH = Path.of(System.getProperty("user.home")+ "/" + "recipe-app"+ File.separator + "attachments" + File.separator);
+    Path BASE_PATH = Path.of(System.getProperty("user.home")+ File.separator + "recipe-app"+ File.separator + "attachments" + File.separator);
 }
